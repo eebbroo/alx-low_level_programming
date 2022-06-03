ae230176082816576dbf81987462d1f44b6f7288
eebbroo <majamibrahim@gmail.com>
2022-06-03 10:19:32 -0700

    A random number script file created

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,8 +11,8 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	printf("%d\n", n);
-	printf("if %d is greater than 0: is positive\n", n);
-	printf("if %d is 0: is zero\n", n);
-	printf("if %d is less than 0: is negative\n", n);
+	printf("%d is positive\n", n);
+	printf("%d is zero\n", n);
+	printf("%d is negative\n", n);
 	return (0);
 }
