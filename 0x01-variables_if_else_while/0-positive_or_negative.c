7df438948abb58871e6b3fe8ba0a14c63eee80c9
eebbroo <majamibrahim@gmail.com>
2022-06-03 10:43:28 -0700

    corrections made to random-number script

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,16 +3,19 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * This programm will assign a random number each time it is executed
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	printf("%d\n", n);
-	printf("%d is positive\n", n);
-	printf("%d is zero\n", n);
-	printf("%d is negative\n", n);
+	printf("%i is positive\n", n);
+	return (0);
+	printf("%i is zero\n", n);
+	return (0);
+	printf("%i is negative\n", n);
 	return (0);
 }
