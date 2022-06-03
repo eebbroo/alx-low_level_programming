d6eebb3e82fec9a30e20fe703ceaa13dc5d4eedd
eebbroo <majamibrahim@gmail.com>
2022-06-03 12:33:21 -0700

    adding the last-digit script file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Program to display the last digit of a randomly generated number
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n, R;
@@ -20,7 +24,7 @@ int main(void)
 	}
 	else if (R < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6\n", n, R);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, R);
 	}
 	return (0);
 }
