6e40f9f0b15daae77f3c662426bab56a411b367c
eebbroo <majamibrahim@gmail.com>
2022-06-07 13:39:18 -0700

    new files added

---
+++ 0x01-variables_if_else_while/TEST.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		printf("%i\n", i);
+	}
+	return (0);
+}
