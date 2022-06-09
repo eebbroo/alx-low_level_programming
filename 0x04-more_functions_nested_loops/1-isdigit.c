daeb2f08e10e21abcb195452687ab82faf400958
eebbroo <majamibrahim@gmail.com>
2022-06-09 09:38:29 -0700

    new files

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**
  * main - function that checks for a digit (0 through 9).
  *
- * Return: 1 if c is a digit or 0 otherwise
+ * Return: 1 if c is a digit bwtn (0 and 9) OR 0 if otherwise
  */
 
 int _isdigit(int c)
 {
-	if(c >= 0 && c <= 9)
+	if(c >= 48 && c <= 57)
 	{
 		return (1);
 	}
