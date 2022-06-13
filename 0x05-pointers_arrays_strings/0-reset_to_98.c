10e551a53837bf2a8c1bec5b6fa70217eecbc836
eebbroo <majamibrahim@gmail.com>
2022-06-13 06:54:01 -0700

    0-reset_to_98.c file added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *reset_to_98 - Function takes a pointer to a parameter and updates it to 98.
+ *
+ *@n: A pointer to an address
+ *
+ *Return: return nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
