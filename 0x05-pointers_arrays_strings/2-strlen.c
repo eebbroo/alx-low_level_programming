f70e8ec3aa6ade2139507ef6ce5594b4fdd83cee
eebbroo <majamibrahim@gmail.com>
2022-06-13 15:30:24 -0700

    2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - function to return a string
+ *
+ *@s: string to be passed to function
+ *
+ * Return: return (c)
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+	
+	while(s[c])
+	{
+		c++;
+	}
+	return (c);
+}
