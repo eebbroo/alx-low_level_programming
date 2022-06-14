a2a8a9c2657b387a55346dca579531544cd9fa34
eebbroo <majamibrahim@gmail.com>
2022-06-13 22:10:44 -0700

    4-print_rev.c file added

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -27,12 +27,3 @@ void print_rev(char *s)
 	}
 	putchar('\n');
 }
-
-int main(void)
-{
-    char *str;
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
-    return (0);
-}
