d899a3d3f300a767bbff29f257ca4f3539cd62c8
eebbroo <majamibrahim@gmail.com>
2022-06-14 15:49:26 -0700

    addition of new files

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - Function that converts strings to integers
+ *
+ * @s - input string
+ *
+ * Return: Non.
+ */
+
+int _atoi(char *s)
+{
+	int x;
+	char str
+
+	*s = str;
+       	x = atoi(*s);
+}
