5dac65de8cff48bb18fc741188af734ee0ba7f67
eebbroo <majamibrahim@gmail.com>
2022-06-14 00:51:10 -0700

    new file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *puts2 - Funtion that prints any other character from string
+ *
+ *@str: parameter used
+ *
+ * Return: No return
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		putchar (str[i]);
+		putchar ('\n');
+	}
+}
