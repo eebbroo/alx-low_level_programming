3c56156245190d272aefb687ae19809d1c095d7c
eebbroo <majamibrahim@gmail.com>
2022-06-20 14:14:55 -0700

    New files added

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,34 +9,18 @@
  *
  * Return: the pointer to the first occurrence of the character c.
  */
+
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) == c)
+		if (s[i] == c)
 		{
 			return (s + i);
 		}
-		else
-		{
-			break;
-		}
 	}
-	return('\0');
-}
 
-int main(void)
-{
-    char *s = "helclo";
-    char *f;
-
-    f = _strchr(s, 'c');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
+	return ('\0');
 }
