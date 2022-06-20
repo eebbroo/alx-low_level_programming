fd3b750c9b2d715a08ab3f03e475cbdee9df0f21
eebbroo <majamibrahim@gmail.com>
2022-06-20 12:50:00 -0700

    New files

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * infinite_add - adds two numbers
- *
+ *infinite_add - adds two numbers
  * @n1: number one.
  * @n2: number two.
  * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
+ * @size_r: buffer size
  *
  * Return: the pointer to dest.
  */
@@ -14,11 +13,15 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
-
+	
 	while (*(n1 + c1) != '\0')
+	{
 		c1++;
+	}
 	while (*(n2 + c2) != '\0')
+	{
 		c2++;
+	}
 	if (c1 >= c2)
 		bg = c1;
 	else
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Here are more function on pointers and arrays in C. written by me(Majam Ibrahim) during my stude to become a fullstack SE with ALX
