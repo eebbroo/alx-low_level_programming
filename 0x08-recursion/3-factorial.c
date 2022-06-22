f674e85f8c32f10869f37010351a3c5dac7e8423
eebbroo <majamibrahim@gmail.com>
2022-06-22 13:56:33 -0700

    New factorial function file added

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *factorial - function that returns factorial of a number
+ *
+ *@n: Integer parameter
+ *
+ *Return: Return factorial(fact_num)
+ */
+
+int factorial(int n)
+{
+	int fact_num;
+
+	if (n == 1)
+	{
+		return (1);
+	}
+	else if (n != 0)
+	{
+		fact_num = n * factorial(n - 1);
+	}
+	return (fact_num);
+}
