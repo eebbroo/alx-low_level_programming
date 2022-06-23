f7fe3115c151700a1470983c0bdefcb77c5843cf
eebbroo <majamibrahim@gmail.com>
2022-06-22 17:28:54 -0700

    new file added

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_sqrt_recursion - function that returns the natural square root of a number
+ *
+ * @n: a parameter number
+ *
+ * Return: return square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if(1)
+	{
+	
+	}
+
+}
+
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - returns 1 if input integer is prime number
+ * 
+ * @n: input integer
+ *
+ * Return: return(1) if TRUE else, return (0).
+ */
+
+int is_prime_number(int n)
+{
+	int div;
+
+	if ((n / div) == 1 && .)	{
+		return (1);
+	}
+	else if (n == 1)
+		return (0);
+	else if ((n % 2) == 0)
+		return (0);
+}
