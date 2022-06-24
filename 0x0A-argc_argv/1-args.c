1c06204d0c4a3c3f49673bebe5dcd7926c60f3ce
eebbroo <majamibrahim@gmail.com>
2022-06-24 10:52:26 -0700

    New function file added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Aprogram that prints the number of arguments passed into it
+ *
+ *@argc: Number of arguments passed
+ *@argv: Array pointers to string parameters
+ *
+ * Return: Always return(0)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (*argv)
+		printf("%d\n", argc);
+	return (0);
+}
