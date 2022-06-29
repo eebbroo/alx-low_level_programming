9211db712f8996324af572e87ac8dad8ff0e95c5
eebbroo <majamibrahim@gmail.com>
2022-06-28 18:41:20 -0700

    Correction on file 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,7 +28,9 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		return (s1);
 	if (s1 == NULL && s2 == NULL)
+	{
 		;
+	}
 	mem = (char *)malloc(sizeof(char) * (i + j + 1));
 	if (mem == NULL)
 	{
