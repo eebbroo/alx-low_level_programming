6a12b9cd452baac6a644219874637605bd2c2e43
eebbroo <majamibrahim@gmail.com>
2022-06-30 02:50:54 -0700

    New file added

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - function that concat all arguments passed
+ *
+ * @ac: numbers of argument
+ * @av: argument arrays
+ *
+ * Return: returns concatenated mem (SUCCESS), NULL (ERROR)
+ */
+
+
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, len;
+	char *mem;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+		len++;
+	}
+
+	mem = malloc(sizeof(char) * (len + 1));
+	if (mem == NULL)
+		return (NULL);
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			mem[k] = av[i][j];
+			k++;
+		}
+		mem[k] = '\n';
+		k++;
+	}
+	return (str);
+}
