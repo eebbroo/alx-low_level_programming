8b5e3b8977b755b450bc4228518eddfcfbad3e53
eebbroo <majamibrahim@gmail.com>
2022-07-04 05:26:02 -0700

    corrections

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main- function that prints name of file compiled from
+ *
+ * @void: NULL parameter
+ *
+ * Return: return 0.
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,6 @@
 #ifndef 3_FUNCTION_LIKE_MACRO_H
 #define 3_FUNCTION_LIKE_MACRO_H
+#include <stdlib.h>
 
 #define ABS(x) abs(x)
 
