1bd38202b4a5bdc973e0c5014986775389c930cc
eebbroo <majamibrahim@gmail.com>
2022-07-05 06:14:27 -0700

    New corrections on .h-file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,6 @@
  * @age: Dog age
  * @owner: Owner of dog
  */
-
 struct dog
 {
 	char *name;
@@ -19,7 +18,6 @@ struct dog
 /**
  * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -27,4 +25,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*_DOG_H_*/
+#endif /*DOG_H*/
