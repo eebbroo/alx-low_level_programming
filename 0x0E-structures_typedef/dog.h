bb3285d8fa2b20c25184606d076a8a22415957fb
eebbroo <majamibrahim@gmail.com>
2022-07-05 05:50:48 -0700

    New corrections on .h-file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,22 +11,21 @@
  * Description: struct called "dog" that stores its name, its age
  * and the name of its owner.
  */
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+};
 
-typedef struct dog dog_t;
 /**
  * dog_t - Typedef for struct dog
  */
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /*_DOG_H_*/
