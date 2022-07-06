3dc89a8b90b4d57c42e24ca128abed63489ecd61
eebbroo <majamibrahim@gmail.com>
2022-07-06 12:43:37 -0700

    New files

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,12 @@
+#ifndef _CALC_H_
+#define _CALC_H_
+
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+
+#endif
