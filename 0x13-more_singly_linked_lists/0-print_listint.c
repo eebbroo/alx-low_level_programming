8d9412ee9427fdf6097b0255a9e373d2c323991d
eebbroo <majamibrahim@gmail.com>
2022-07-18 07:54:48 -0700

    Print-list funtion file created

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint- Fucntion that returns the number of elements in a linked
+ *
+ * @h: pointer to the first node
+ *
+ * Return: Return the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp = h;
+	size_t count, values;
+
+	count = 0;
+	while (temp != NULL)
+	{
+		values = temp->n;
+		printf("%lu\n", values);
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
