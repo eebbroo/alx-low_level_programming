ba62785163bc9312c23088a24970647d005cf26f
eebbroo <majamibrahim@gmail.com>
2022-07-22 07:50:56 -0700

    bug fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Program that prints all possible combinations of two two-digit numbers.
+ * main - Program that prints all possible
+ * combinations of two two-digit numbers.
  *
  * Return: Always 0 (Success)
  */
